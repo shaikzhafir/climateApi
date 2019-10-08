4642fc010db9458770e19b6c778e5bc445794146
bobross96 <zadus96@gmail.com>
2019-10-08 15:11:02 +0800

    updated api to air quality

--- blink.js
+++ blink.js
@@ -1,47 +1,23 @@
 'use strict';
 
-//code that is used to grab the data from the api 
-const ISS_API = "https://api.wheretheiss.at/v1/satellites/25544"
+//let airApi = require('./iss.js');
 
 const fetch = require('node-fetch');
 
-var foo = "bar";
+const AQI_API = "https://api.waqi.info/feed/singapore/?token=21f0d11cf0d4752c65ea4d1520d3544966a92c42"
 
-const get_data = async url => {
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-   // console.log(json);
-    foo = json.altitude;
-    //console.log(foo);
-    //console.log(latitude);
-  } catch (error) {
-    console.log(error);
-  }
-};
+let airQuality = 20;
 
-get_data(ISS_API);
-
-
-
-/* async function getISS() {
-    const response = await fetch(ISS_API);
+async function getAir(){
+    const response = await fetch("https://api.waqi.info/feed/singapore/?token=21f0d11cf0d4752c65ea4d1520d3544966a92c42");
     const data = await response.json();
-    console.log(data.altitude);
-    const { latitude, longitude} = data; //javascript destrucutirng 
-    const latitude = data.latitude;
-    
-    console.log(latitude);
-    console.log(longitude);
-    
-    document.getElementById('lat').textContent = latitude;
-    document.getElementById('lon').textContent = longitude;
-
+    airQuality = data.data.aqi;
+    console.log(airQuality);
 }
 
-getISS();
-console.log(data);
- */
+ getAir();
+ 
+
 
 
 const {
@@ -51,25 +27,32 @@ const five = require('johnny-five');
 const Led = require('johnny-five');
 const board = new five.Board({   //intitalise board using the internet client host 
   port: new EtherPortClient({
-    host: '192.168.137.128',
+    host: '192.168.137.79',
     port: 3030
   }),
   repl: false
 });
 
-const LED_PIN = 0;
+const LED_PIN = 13;
 
 
 board.on("ready", () => {
     const led = new five.Led(LED_PIN);
-  
-    led.pulse();
-    get_data(ISS_API);
-    console.log(foo + "im here");
+    //console.log(brightness);
+    console.log(airQuality);
+    led.pulse({
+      easing: "linear",
+      duration: 1000,
+      cuePoints: [0, 1],
+      keyFrames: [0, airQuality],
+      onstop: function() {
+        console.log("Animation stopped");
+      }
+    });
     
   // Stop and turn off the led pulse loop after
   // 10 seconds (shown in ms)
-  board.wait(10000, () => {
+  board.wait(20000, () => {
     
     // stop() terminates the interval
     // off() shuts the led off
--- iss.js
+++ iss.js
@@ -1,9 +1,9 @@
 
-
+const fetch = require('node-fetch');
 
 const ISS_API = "https://api.wheretheiss.at/v1/satellites/25544"
 
-setInterval(getISS, 500);
+//setInterval(getISS, 500);
 
 
 async function getISS() {
@@ -11,7 +11,7 @@ async function getISS() {
     const data = await response.json();
     console.log(data.altitude);
     const { latitude, longitude} = data; //javascript destrucutirng 
-    const latitude = data.latitude;
+    
     
     console.log(latitude);
     console.log(longitude);
@@ -21,7 +21,31 @@ async function getISS() {
 
 }
 
-getISS();
+//getISS();
+let country = "singapore";
+
+const AQI_API = "https://api.waqi.info/feed/singapore/?token=21f0d11cf0d4752c65ea4d1520d3544966a92c42"
+
+async function getAir(){
+    const response = await fetch(AQI_API);
+    const data = await response.json();
+    const airQuality = data.data.aqi;
+    console.log(airQuality + "here");
+}
+
+//getAir();
+
+module.exports = {
+    getAir,
+    AQI_API
+
+}
 
 
-exports.getISS = getISS();
+
+
+
+
+
+
+
